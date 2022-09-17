e21e1d2ca6284e7d41fc8201db207d0ada66fb94
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-18 00:23:23 +0200

    Test post model validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  subject do
+    author = User.new(
+      name: 'Tom',
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'Teacher from Mexico.',
+      posts_counter: 0
+    )
+
+    post = Post.new(
+      author: author,
+      title: 'Hello',
+      text: 'This is my first post',
+      comments_counter: 0,
+      likes_counter: 0
+    )
+
+    Comment.create(
+      text: 'comment',
+      post: post,
+      author: author
+    )
+  end
+
+  describe 'Comments' do
+  it 'should update comments counter' do
+    expect(subject.send(:comment_counter)).to be_truthy
+  end
+end
 
 end
