91646e15950929b9d52316bd0be40d176ca745bd
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-18 00:23:00 +0200

    Add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :update_posts_counter
 
   def update_posts_counter
@@ -10,6 +15,6 @@ class Post < ApplicationRecord
   end
 
   def five_recent_comments
-    comments.order(created_at: :desc).limit(5)
+    Comment.where(post: self).limit(5)
   end
 end
