88934acd19d40a1aee4ad2e64726e66624a8dfb1
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-20 21:34:04 +0200

    Add posts controller tests

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /id' do
+  context 'GET /id' do
     it 'returns http success' do
-      get '/posts/id'
+      get '/users/:user_id/posts/:id'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render index template' do
+      get user_posts_path(1)
+      expect(response).to render_template('index')
+    end
+
+    it 'should render correct text' do
+      get user_posts_path(1)
+      expect(response.body).to include('Posts#id')
     end
   end
 
-  describe 'GET /show' do
+  context 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
+      get '/users/:user_id/posts'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render show template' do
+      get user_post_path(1, 2)
+      expect(response).to render_template(:show)
+    end
+
+    it 'should render correct text' do
+      get user_post_path(1, 2)
+      expect(response.body).to include('These are the posts')
     end
   end
 end
