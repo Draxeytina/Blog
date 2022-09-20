f78bc1d46022f9d03971f09433ec3187c46067ba
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-20 17:04:21 +0200

    Apply requested changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  def id
+  def index
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  def id
+  def index
   end
 
   def show
