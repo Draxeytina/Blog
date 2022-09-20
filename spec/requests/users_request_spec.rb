5a4efe79f0e7f49721d270a9ae296ec338a49aef
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-20 21:32:15 +0200

    Add users controller tests

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /id' do
+  context 'GET /id' do
     it 'returns http success' do
       get '/users/id'
       expect(response).to have_http_status(:success)
     end
+
+      it 'should render index template' do
+        get users_path
+        expect(response).to render_template('index')
+    end
+
+    it 'should render text in template' do
+        get users_path
+        expect(response.body).to include('Users#id')
+    end
   end
 
-  describe 'GET /show' do
+  context 'GET /show' do
     it 'returns http success' do
       get '/users/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should render show template' do
+      get user_path(1)
+      expect(response).to render_template(:show)
+    end
+
+    it 'should render correct text in template' do
+        get user_path(1)
+        expect(response.body).to include('Here are the users')
     end
   end
 end
