ea7cf0642431465d8fb5a24985825d42e2b1d4c1
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-30 00:31:50 +0200

    Update user parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password)
+    end
+    devise_parameter_sanitizer.permit(:sign_in) do |u|
+      u.permit(:username, :email)
+    end
+  end
 end
