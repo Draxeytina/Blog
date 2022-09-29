e91463a51a3edb9d8b3223e7146350767e6da701
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-30 00:44:04 +0200

    Update views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   has_many :posts
   has_many :comments
   has_many :likes
@@ -10,7 +11,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  after_initialize :update_counter
+  after_initialize :update_counter, :set_img_tag
 
   def three_recent_posts
     Post.where(author: self).order(updated_at: :desc).limit(3)
@@ -22,5 +23,9 @@ class User < ApplicationRecord
 
   def update_counter
     self.posts_counter ||= 0
+  end
+
+  def set_img_tag
+    self.photo ||= 'https://i.stack.imgur.com/YQu5k.png'
   end
 end
