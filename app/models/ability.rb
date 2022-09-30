3ed2c7955317d19c03396c48f0662346d382827c
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-30 22:59:17 +0200

    Update CanCanCan

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Post, author: user
+    can :manage, Comment, author: user
+    return unless user.role == 'admin'
+
+    can :manage, :all
+  end
+end
