0431dc80ba6f06217e557dc0621c08246f32762b
Godwin Nwachukwu <nwgwin@gmail.com>
2022-09-30 23:05:12 +0100

    remove unused code

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,4 @@ Rails.application.routes.draw do
   end
   resources :posts, only: [:new, :create, :destroy]
   resources :likes, only: [:create]
-
-  # resources :users, only: [:index, :show] do
-  #   resources :posts, only: [:index, :new, :create, :show, :destroy] do
-  #     resources :comments, only: [:new, :create, :destroy]
-  #     resources :likes, only: [:create]
-  #   end
-  # end
-
 end
